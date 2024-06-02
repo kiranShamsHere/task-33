af9ddf570e303ece3b4e35a1af6152e2d7a2ae32
Kiran Shams <160856436+kiranShamsHere@users.noreply.github.com>
2024-06-02 01:41:37 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task-33</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <p id="para">
+        <?php
+
+        $num = 1;
+        do {
+            echo $num . "<br>";
+            $num++;
+        } while ($num <= 5);
+        ?>
+    </p>
+</body>
+
+</html>
